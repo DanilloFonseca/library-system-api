7a075a9919a6f4bc78a8d982f302618e9e2eb455
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2025-03-11 23:46:13 -0300

    refactor: remove UserController, User and UserRepository

--- library-api/src/main/java/com/fonseca/library_api/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fonseca.library_api.repository;
-
-import com.fonseca.library_api.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
